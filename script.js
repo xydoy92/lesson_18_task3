dcdfb93f2c4ac5566938cfa467e9bdcdd0b04c65
xydoy92 <xydoy92@gmail.com>
2020-08-03 14:22:39 +0300

    feat: random Color added

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let lis = document.body. querySelectorAll("li");
 let liAmount = document.body. querySelectorAll("li").length;
 
-function changeColorEverySecond(array, arrayLength) {
-	let randomNumber = Math.random();
-	while(randomNumber < arrayLength) {
-		randomNumber *=10;
-	}
-	randomNumber = Math.floor(randomNumber);
-	randomNumber = randomNumber % arrayLength;
-	array[randomNumber].className = "red";
+function changeColorEverySecond(array, number) {
+	let randomNumber = randomTo(number);
+	array[randomNumber].style.color = changeColor(256);
+	console.log(array[randomNumber].style.color);
 	setTimeout(() => {
-		array[randomNumber].className = "";
-		changeColorEverySecond(array, arrayLength);
+		array[randomNumber].style.color = "";
+		changeColorEverySecond(array, number);
 	}, 1000);
+}
+
+function changeColor(number) {
+	let red = getRGB(number);
+	let green = getRGB(number);
+	let blue = getRGB(number);
+	return `#${red}${green}${blue}`;
+}
+
+function randomTo(number) {
+	return Math.floor(Math.random()*number);
+}
+
+function getRGB(number) {
+	let rgb = randomTo(number).toString(16);
+	if(!rgb[1]) {
+		rgb = `0${rgb}`;
+	}
+	return rgb;
 }
 
 changeColorEverySecond(lis, liAmount);
